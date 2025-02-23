7f1fdcbc962bc6ccc890acab0cd3112b320621ad
kefir228 <oleg05karp@gmail.com>
2025-02-23 12:48:01 +0200

    add tasks to localStorage

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -2,8 +2,10 @@
 
 import { RxCross2 } from "react-icons/rx";
 import { useForm } from 'react-hook-form'
-import { openModal } from "@/store/modalSlice";
-import { useDispatch } from "react-redux";
+import { openModal, addTask } from "@/store/modalSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+
 
 interface FormData {
     title: string
@@ -14,7 +16,7 @@ interface FormData {
 
 export default function Modal() {
     const dispatch = useDispatch()
-
+    const { isOpen } = useSelector((state: RootState) => state.modal)
     const {
         register,
         handleSubmit,
@@ -23,7 +25,9 @@ export default function Modal() {
     } = useForm<FormData>()
 
     const onSumbit = (data: FormData) => {
+        dispatch(addTask(data))
         reset()
+        closeModal()
         console.log('Form Data', data);
     }
 
@@ -32,61 +36,65 @@ export default function Modal() {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSumbit)}
-            className="max-w-md mx-auto p-6 bg-white shadow-md rounded-lg"
-        >
-            <div className="mb-4 flex items-center justify-between">
-                <label className="block font-semibold">Додати нову подію</label>
-                <button onClick={closeModal}>
-                    <RxCross2 />
-                </button>
-            </div>
+        isOpen && (
+            <div className="flex fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-[1000]">
+                <form onSubmit={handleSubmit(onSumbit)}
+                    className="max-w-md w-full mx-auto p-6 bg-white shadow-md rounded-lg"
+                >
+                    <div className="mb-4 flex items-center justify-between">
+                        <label className="block font-semibold">Додати нову подію</label>
+                        <button onClick={closeModal}>
+                            <RxCross2 />
+                        </button>
+                    </div>
 
-            <div>
-                <input
-                    {...register('title', { required: "Назва обов'язкова" })}
-                    className="w-full p-2 border border-gray-300 rounded mt-1"
-                    placeholder="Введіть назву"
-                />
-            </div>
+                    <div>
+                        <input
+                            {...register('title', { required: "Назва обов'язкова" })}
+                            className="w-full p-2 border-b border-gray-300 mt-1"
+                            placeholder="Введіть назву"
+                        />
+                    </div>
 
-            <div>
-                <textarea
-                    {...register('description', {
-                        required: "Опис є обов'язковим",
-                        minLength: { value: 10, message: "Опис має містити мінімум 10 символів" }
-                    })}
-                    className="w-full p-2 border border-gray-300 rounded mt-1"
-                    placeholder="Введіть опис..."
-                    rows={4}
-                />
-            </div>
+                    <div>
+                        <textarea
+                            {...register('description', {
+                                required: "Опис є обов'язковим",
+                                minLength: { value: 10, message: "Опис має містити мінімум 10 символів" }
+                            })}
+                            className="w-full p-2 border-b border-gray-300 mt-1"
+                            placeholder="Введіть опис..."
+                            rows={4}
+                        />
+                    </div>
 
-            <div className="flex items-center justify-between">
-                <input
-                    type="date"
-                    {...register("date", {
-                        required: "Дата є обов'язковою",
-                        validate: (value) => (new Date(value) >= new Date() ? true : "Дата не може бути в минулому"),
-                    })}
-                    className="p-2 border border-gray-300 rounded mt-1"
-                />
-                {errors.date && <p className="text-red-500 text-sm">{errors.date.message}</p>}
+                    <div className="flex items-center justify-between flex-wrap">
+                        <input
+                            type="date"
+                            {...register("date", {
+                                required: "Дата є обов'язковою",
+                                validate: (value) => (new Date(value) >= new Date() ? true : "Дата не може бути в минулому"),
+                            })}
+                            className="p-2 border-b border-gray-300 mt-1"
+                        />
+                        {errors.date && <p className="text-red-500 text-sm">{errors.date.message}</p>}
 
-                <input
-                    type="time"
-                    {...register("time", {
-                        required: "Час є обов'язковим",
-                        validate: (value) => (value ? true : "Будь ласка, оберіть час"),
-                    })}
-                    className=" p-2 border border-gray-300 rounded mt-1"
-                />
-                {errors.time && <p className="text-red-500 text-sm">{errors.time.message}</p>}
-            </div>
+                        <input
+                            type="time"
+                            {...register("time", {
+                                required: "Час є обов'язковим",
+                                validate: (value) => (value ? true : "Будь ласка, оберіть час"),
+                            })}
+                            className=" p-2 border-b border-gray-300 mt-1"
+                        />
+                        {errors.time && <p className="text-red-500 text-sm">{errors.time.message}</p>}
+                    </div>
 
-            <button type="submit">
-                SAVE
-            </button>
-        </form>
+                    <button type="submit" className="bg-blue-500 p-1 rounded mt-2">
+                        SAVE
+                    </button>
+                </form>
+            </div >
+        )
     )
 }
--- src/store/modalSlice.ts
+++ src/store/modalSlice.ts
@@ -1,25 +1,45 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+interface FormData {
+    title: string
+    description: string
+    date: string
+    time: string
+}
+
 interface modalState {
     isOpen: boolean
     type: 'calendar' | 'modal'
+    tasks: FormData[]
+}
+
+const getSavedTask = ():FormData[] => {
+    if(typeof window !== 'undefined') {
+        return JSON.parse(localStorage.getItem('task')||'[]')
+    }
+    return []
 }
 
 const initialState: modalState = {
     isOpen: false,
-    type: 'modal'
+    type: 'modal',
+    tasks: getSavedTask()
 }
 
 const modalSlice = createSlice({
     name: 'modal',
     initialState,
     reducers: {
-        openModal: (state, action: PayloadAction<{isOpen: boolean, type: 'calendar' | 'modal'}>) => {
+        openModal: (state, action: PayloadAction<{ isOpen: boolean, type: 'calendar' | 'modal' }>) => {
             state.isOpen = action.payload.isOpen
             state.type = action.payload.type
+        },
+        addTask: (state, action: PayloadAction<FormData>) => {
+            state.tasks.push(action.payload)
+            localStorage.setItem('tasks', JSON.stringify(state.tasks))
         }
     }
 })
 
-export const { openModal } = modalSlice.actions
+export const { openModal, addTask } = modalSlice.actions
 export default modalSlice.reducer
